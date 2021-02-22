37b72ad2298e3de21fc223649adb7c33fef048ca
DoLT <dolt@runsystem.net>
2021-02-22 10:25:46 +0700

    add CORS proxy

--- express/server.js
+++ express/server.js
@@ -1,22 +1,22 @@
-'use strict';
-const express = require('express');
-const path = require('path');
-const serverless = require('serverless-http');
+"use strict";
+const express = require("express");
+const serverless = require("serverless-http");
 const app = express();
-const bodyParser = require('body-parser');
+const cors_proxy = require("cors-anywhere");
 
-const router = express.Router();
-router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
-  res.end();
+app.get("/", (req, res) => {
+  res.send("Welcome to DoLT CORS!");
 });
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-router.post('/', (req, res) => res.json({ postBody: req.body }));
-
-app.use(bodyParser.json());
-app.use('/.netlify/functions/server', router);  // path must route to lambda
-app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+let proxy = cors_proxy.createServer({
+  originWhitelist: [], // Allow all origins
+  requireHeaders: [], // Do not require any headers.
+  removeHeaders: [], // Do not remove any headers.
+});
+/* Attach our cors proxy to the existing API on the /proxy endpoint. */
+app.get("/proxy/:proxyUrl*", (req, res) => {
+  req.url = req.url.replace("/proxy/", "/"); // Strip '/proxy' from the front of the URL, else the proxy won't work.
+  proxy.emit("request", req, res);
+});
 
 module.exports = app;
 module.exports.handler = serverless(app);
